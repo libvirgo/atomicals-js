63112ff5d889c7e19ef2e50b8c178d6c1e8a0d10
laterntian <laterntian@gmail.com>
2024-01-01 15:21:55 +0800

    env args to cli args.

--- lib/commands/wallet-init-command.ts
+++ lib/commands/wallet-init-command.ts
@@ -1,25 +1,26 @@
-import { CommandResultInterface } from "./command-result.interface";
-import { CommandInterface } from "./command.interface";
-import { createPrimaryAndFundingImportedKeyPairs } from "../utils/create-key-pair";
-import { jsonFileExists, jsonFileWriter } from "../utils/file-utils";
-import { walletPathResolver } from "../utils/wallet-path-resolver";
+import {CommandResultInterface} from "./command-result.interface";
+import {CommandInterface} from "./command.interface";
+import {createPrimaryAndFundingImportedKeyPairs} from "../utils/create-key-pair";
+import {jsonFileExists, jsonFileWriter} from "../utils/file-utils";
 import * as fs from 'fs';
 
-const walletPath = walletPathResolver();
 
 export class WalletInitCommand implements CommandInterface {
-    constructor(private phrase: string | undefined, private path: string, private n?: number) {
+    constructor(private phrase: string | undefined, private path: string, private filepath: string, private filename: string, private n?: number) {
 
     }
+
+    walletPath = `${this.filepath}/${this.filename}`
+
     async run(): Promise<CommandResultInterface> {
         if (await this.walletExists()) {
             throw "wallet.json exists, please remove it first to initialize another wallet. You may also use 'wallet-create' command to generate a new wallet."
         }
 
-        const { wallet, imported } = await createPrimaryAndFundingImportedKeyPairs(this.phrase, this.path, this.n);
-        const walletDir = `wallets/`;
+        const {wallet, imported} = await createPrimaryAndFundingImportedKeyPairs(this.phrase, this.path, this.n);
+        const walletDir = `${this.filepath}/`;
         if (!fs.existsSync(walletDir)) {
-          fs.mkdirSync(walletDir);
+            fs.mkdirSync(walletDir);
         }
         const created = {
             phrase: wallet.phrase,
@@ -35,14 +36,15 @@ export class WalletInitCommand implements CommandInterface {
             },
             imported
         };
-        await jsonFileWriter(walletPath, created);
+        await jsonFileWriter(this.walletPath, created);
         return {
             success: true,
             data: created
         }
     }
+
     async walletExists() {
-        if (await jsonFileExists(walletPath)) {
+        if (await jsonFileExists(this.walletPath)) {
             return true;
         }
     }
