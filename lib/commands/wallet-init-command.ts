bb9d42e31762c9c9a5921a0982544d0e89b2b2d4
libvirgo <sakurapetgirl@live.com>
2024-01-10 08:27:57 +0800

    [fix] add network while create funding wallet.

--- lib/commands/wallet-init-command.ts
+++ lib/commands/wallet-init-command.ts
@@ -1,25 +1,27 @@
-import {CommandResultInterface} from "./command-result.interface";
-import {CommandInterface} from "./command.interface";
-import {createPrimaryAndFundingImportedKeyPairs} from "../utils/create-key-pair";
-import {jsonFileExists, jsonFileWriter} from "../utils/file-utils";
+import { CommandResultInterface } from './command-result.interface';
+import { CommandInterface } from './command.interface';
+import { createPrimaryAndFundingImportedKeyPairs } from '../utils/create-key-pair';
+import { jsonFileExists, jsonFileWriter } from '../utils/file-utils';
 import * as fs from 'fs';
+import { Network } from 'bitcoinjs-lib';
+import { NETWORK } from './command-helpers';
 
 
 export class WalletInitCommand implements CommandInterface {
-    constructor(private phrase: string | undefined, private path: string, private filepath: string, private filename: string, private n?: number, private needFile:boolean = true) {
+    constructor(private phrase: string | undefined, private path: string, private filepath: string, private filename: string, private n?: number, private needFile: boolean = true) {
 
     }
 
-    walletPath = `${this.filepath}/${this.filename}`
+    walletPath = `${this.filepath}/${this.filename}`;
 
-    async run(): Promise<CommandResultInterface> {
+    async run(network: Network = NETWORK): Promise<CommandResultInterface> {
         if (this.needFile) {
             if (await this.walletExists()) {
-                throw "wallet.json exists, please remove it first to initialize another wallet. You may also use 'wallet-create' command to generate a new wallet."
+                throw 'wallet.json exists, please remove it first to initialize another wallet. You may also use \'wallet-create\' command to generate a new wallet.';
             }
         }
 
-        const {wallet, imported} = await createPrimaryAndFundingImportedKeyPairs(this.phrase, this.path, this.n);
+        const { wallet, imported } = await createPrimaryAndFundingImportedKeyPairs(this.phrase, this.path, this.n);
         const created = {
             phrase: wallet.phrase,
             primary: {
@@ -44,7 +46,7 @@ export class WalletInitCommand implements CommandInterface {
         return {
             success: true,
             data: created
-        }
+        };
     }
 
     async walletExists() {
